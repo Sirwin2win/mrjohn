a85b5498b9dadf7ae338565408960e7668026ff4
Ekpe Godwin <sirwin2win@gmail.com>
2025-07-23 17:01:05 +0100

    Evenet and evenlistener and also conditional ststaments

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Eventing from "../components/Eventing";
+import Conditional from "../components/Conditional";
 
 const Blog = () => {
   return (
     <div>
+      <Conditional />
+      <Eventing />
       <h1>Blog Page</h1>
     </div>
   );
